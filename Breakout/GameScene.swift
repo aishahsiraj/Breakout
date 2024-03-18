d302e0b3455a64490f75b27bbfb1c7d50946ae97
Aishah <aishah122005@gmail.com>
2024-03-18 09:45:29 -0500

    Initial Commit

---
+++ Breakout/GameScene.swift
@@ -0,0 +1,16 @@
+//
+//  GameScene.swift
+//  Breakout
+//
+//  Created by Aishah Siraj on 3/18/24.
+//
+
+import SpriteKit
+import GameplayKit
+
+class GameScene: SKScene {
+    
+    override func didMove(to view: SKView) {
+        // this stuff happens once (when the app opens
+    }
+}
